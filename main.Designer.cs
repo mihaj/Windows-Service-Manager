62fed310ba6bc943cca5f82194f28c4af7725011
Miha Jakovac <miha.jakovac@gmail.com>
2016-05-26 13:22:02 +0200

    Add project files.

---
+++ main.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsServiceDashboard
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_restart = new System.Windows.Forms.Button();
+            this.btn_install = new System.Windows.Forms.Button();
+            this.btn_uninstall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.serviceList = new System.Windows.Forms.ComboBox();
+            this.btn_kill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(109, 162);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(93, 32);
+            this.btn_stop.TabIndex = 0;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(10, 162);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(93, 32);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_restart
+            // 
+            this.btn_restart.Location = new System.Drawing.Point(208, 162);
+            this.btn_restart.Name = "btn_restart";
+            this.btn_restart.Size = new System.Drawing.Size(83, 32);
+            this.btn_restart.TabIndex = 2;
+            this.btn_restart.Text = "Restart";
+            this.btn_restart.UseVisualStyleBackColor = true;
+            this.btn_restart.Click += new System.EventHandler(this.btn_restart_Click);
+            // 
+            // btn_install
+            // 
+            this.btn_install.Location = new System.Drawing.Point(10, 124);
+            this.btn_install.Name = "btn_install";
+            this.btn_install.Size = new System.Drawing.Size(93, 32);
+            this.btn_install.TabIndex = 3;
+            this.btn_install.Text = "Install";
+            this.btn_install.UseVisualStyleBackColor = true;
+            this.btn_install.Click += new System.EventHandler(this.btn_install_Click);
+            // 
+            // btn_uninstall
+            // 
+            this.btn_uninstall.Location = new System.Drawing.Point(109, 124);
+            this.btn_uninstall.Name = "btn_uninstall";
+            this.btn_uninstall.Size = new System.Drawing.Size(93, 32);
+            this.btn_uninstall.TabIndex = 4;
+            this.btn_uninstall.Text = "Uninstall";
+            this.btn_uninstall.UseVisualStyleBackColor = true;
+            this.btn_uninstall.Click += new System.EventHandler(this.btn_uninstall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Windows Service manager.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(62, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Services Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsServiceDashboard.Properties.Resources.cog;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(46, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 207);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(303, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(42, 17);
+            this.status.Text = "Status:";
+            // 
+            // serviceList
+            // 
+            this.serviceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serviceList.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.serviceList.FormattingEnabled = true;
+            this.serviceList.Location = new System.Drawing.Point(10, 79);
+            this.serviceList.Name = "serviceList";
+            this.serviceList.Size = new System.Drawing.Size(281, 33);
+            this.serviceList.TabIndex = 9;
+            this.serviceList.SelectedIndexChanged += new System.EventHandler(this.serviceList_SelectedIndexChanged);
+            // 
+            // btn_kill
+            // 
+            this.btn_kill.Enabled = false;
+            this.btn_kill.Location = new System.Drawing.Point(208, 124);
+            this.btn_kill.Name = "btn_kill";
+            this.btn_kill.Size = new System.Drawing.Size(83, 32);
+            this.btn_kill.TabIndex = 10;
+            this.btn_kill.Text = "Kill";
+            this.btn_kill.UseVisualStyleBackColor = true;
+            this.btn_kill.Click += new System.EventHandler(this.btn_kill_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 229);
+            this.Controls.Add(this.btn_kill);
+            this.Controls.Add(this.serviceList);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_uninstall);
+            this.Controls.Add(this.btn_install);
+            this.Controls.Add(this.btn_restart);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.btn_stop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Service Dashboard";
+            this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_restart;
+        private System.Windows.Forms.Button btn_install;
+        private System.Windows.Forms.Button btn_uninstall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.ComboBox serviceList;
+        private System.Windows.Forms.Button btn_kill;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsServiceDashboard
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        string serviceName = "";
+        string serviceEXE = "";
+
+        public static void StartService(string serviceName, int timeoutMilliseconds)
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        public static void StopService(string serviceName, int timeoutMilliseconds)
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void RestartService(string serviceName, int timeoutMilliseconds)
+        {
+            ServiceController service = new ServiceController(serviceName);
+            try
+            {
+                int millisec1 = Environment.TickCount;
+                TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+
+                // count the rest of the timeout
+                int millisec2 = Environment.TickCount;
+                timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds - (millisec2 - millisec1));
+
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            StartService(serviceName, 15000);
+            ReloadButtons();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            StopService(serviceName, 15000);
+            ReloadButtons();
+        }
+
+        private void btn_restart_Click(object sender, EventArgs e)
+        {
+            StopService(serviceName, 15000);
+            ReloadButtons();
+            StartService(serviceName, 15000);
+            ReloadButtons();
+        }
+
+        private void btn_install_Click(object sender, EventArgs e)
+        {
+            
+            var serviceExists = ServiceController.GetServices().Any(s => s.ServiceName == serviceName);
+            if (serviceExists)
+            {
+                MessageBox.Show("Service already installed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists(@"c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe"))
+            {
+                MessageBox.Show("InstallUtil.exe does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + serviceEXE))
+            {
+                MessageBox.Show(serviceEXE + " does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo = new ProcessStartInfo(@"c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe", "\"" + AppDomain.CurrentDomain.BaseDirectory + serviceEXE + "\"");
+                proc.Start();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Thread.Sleep(1000);
+            ReloadButtons();
+        }
+
+        private void btn_uninstall_Click(object sender, EventArgs e)
+        {
+            var serviceExists = ServiceController.GetServices().Any(s => s.ServiceName == serviceName);
+            if (!serviceExists)
+            {
+                MessageBox.Show("Service not installed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists(@"c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe"))
+            {
+                MessageBox.Show("InstallUtil.exe does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + serviceEXE))
+            {
+                MessageBox.Show(serviceEXE + " does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo = new ProcessStartInfo(@"c:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe", "/u \"" + AppDomain.CurrentDomain.BaseDirectory + serviceEXE + "\"");
+                proc.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            Thread.Sleep(1000);
+            ReloadButtons();
+        }
+
+        private void ReloadButtons()
+        {
+            var serviceExists = ServiceController.GetServices().Any(s => s.ServiceName == serviceName);
+
+            if (serviceExists == true)
+            {
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    btn_uninstall.Enabled = false;
+                    btn_stop.Enabled = true;
+                    btn_start.Enabled = false;
+                    btn_install.Enabled = false;
+                    btn_restart.Enabled = true;
+
+                    status.Text = "Service is running...";
+                }
+                else
+                {
+                    btn_uninstall.Enabled = true;
+                    btn_stop.Enabled = false;
+                    btn_start.Enabled = true;
+                    btn_install.Enabled = false;
+                    btn_restart.Enabled = false;
+
+                    status.Text = "Service is " + service.Status.ToString() + ".";
+                }
+            }
+            else
+            {
+                btn_uninstall.Enabled = false;
+                btn_stop.Enabled = false;
+                btn_start.Enabled = false;
+                btn_install.Enabled = true;
+                btn_restart.Enabled = false;
+
+                status.Text = "Service not installed.";
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            serviceName = Properties.Settings.Default.ServiceName;
+            serviceEXE = Properties.Settings.Default.ServiceName + ".exe";
+
+            serviceList.Items.Add(Properties.Settings.Default.ServiceName);
+
+            serviceList.SelectedIndex = 0;
+
+            ReloadButtons();
+        }
+
+        private void serviceList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + serviceEXE))
+            {
+                if (MessageBox.Show(serviceEXE + " does not exist. Closing...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+            }
+
+            ReloadButtons();
+        }
+
+
+        private bool KillProcessByID(string serviceExe, bool waitForExit = false)
+        {
+            int procID = 0;
+            Process[] procs = Process.GetProcessesByName(serviceExe);
+
+            Process proc = null;
+
+            if (procs.Length > 0)
+            {
+                proc = procs[0];
+            }
+            else
+            {
+                return false;
+            }
+
+            if (proc != null)
+            {
+                procID = proc.Id;
+            }
+            else
+            {
+                return false;
+            }
+
+            if (procID > 0)
+            {
+                using (Process p = Process.GetProcessById(procID))
+                {
+                    if (p == null || p.HasExited) return false;
+
+                    p.Kill();
+                    if (waitForExit)
+                    {
+                        p.WaitForExit();
+                    }
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void btn_kill_Click(object sender, EventArgs e)
+        {
+            if (KillProcessByID(serviceName))
+            {
+                MessageBox.Show(serviceName + " was terminated.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show(serviceName + " was not terminated.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
